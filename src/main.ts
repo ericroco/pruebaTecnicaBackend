89f975529f98899466b9493a6ae4a1f5f7fe479d
ericroco <erickrodas559@gmail.com>
2026-04-30 13:22:00 -0500

    integración Fullstack

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
